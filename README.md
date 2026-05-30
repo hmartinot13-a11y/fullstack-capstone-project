a0572595ab5bcd8c48a3de4e66895a58ef7b680c
Herve MARTINOT <h.martinot13@gmail.com>
2026-05-30 18:40:45 -0400

    Module 2 commit after step1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # coding-project-template
+
+--Project set up
+git clone https://github.com/hmartinot13-a11y/fullstack-capstone-project.git
+
+cd fullstack-capstone-project
+
+-- To access MongoDB CLI
+//mongosh mongodb://root:ni6VWAfyx6YDGDLOBnSKNTQc@172.21.20.147:27017
+
+--créer le dump 
+//mongodump --uri="mongodb://root:ni6VWAfyx6YDGDLOBnSKNTQc@172.21.20.147:27017" --out=/home/project/fullstack-capstone-project/mongodump
+
+--restaurer la db
+//mongorestore --drop /home/project/fullstack-capstone-project/mongodump
+mongorestore --uri="mongodb://root:ni6VWAfyx6YDGDLOBnSKNTQc@172.21.20.147:27017" --drop /home/project/fullstack-capstone-project/mongodump
--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,20 +9,24 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
+    try {
+      if (dbInstance){
         return dbInstance
-    };
+      };
 
-    const client = new MongoClient(url);      
+      const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+      // Task 1: Connect to MongoDB
+      await client.connect();
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+      // Task 2: Connect to database giftDB and store in variable dbInstance
+      const dbInstance = client.db(dbName);
+      console.log("Connect to giftDB successfull.");
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+      // Task 3: Return database instance
+      return dbInstance;
+      } catch (error) {
+        console.error("Erreur de connexion à MongoDB:", error);
+        throw error;
+      }      
 }
-
 module.exports = connectToDatabase;
