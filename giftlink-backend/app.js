8369cabbe7a9165cb88e07fbe59739bc61994ced
Herve MARTINOT <h.martinot13@gmail.com>
2026-05-31 00:35:34 -0400

    Module 2 commit after step5

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//const searchRoutes = require('./routes/searchRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -38,9 +38,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-//app.use('/api/search', searchRoutes);
-
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
