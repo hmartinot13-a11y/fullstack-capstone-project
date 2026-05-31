418c67379f50e11ac592864e4fdd71cc6309cfb3
Herve MARTINOT <h.martinot13@gmail.com>
2026-05-31 01:12:16 -0400

    module 2 step 5 with logs

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -14,24 +14,28 @@ router.get('/', async (req, res, next) => {
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        console.log("before: " + req.query.name);
+        console.log("before: " + JSON.stringify(req.query.name,null,4));
         if (req.query.name>req.query.name && req.query.name.trim() !== '')
         {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
+            console.log("query.name: " + JSON.stringify(query.name,null,4));
         }
-        console.log("after: " + req.query.name);
+        console.log("after: " + JSON.stringify(req.query.name,null,4));
         // Task 3: Add other filters to the query
         if (req.query.category) {
             // {{insert code here}}
             query.category = req.query.category;
+            console.log("query.category: " + query.category);
         }
         if (req.query.condition) {
             // {{insert code here}} 
             query.condition = req.query.condition;
+            console.log("query.condition: " + query.condition);
         }
         if (req.query.age_years) {
             // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
+            console.log("query.age_years: " + JSON.stringify(query.age_years,null,4));
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
