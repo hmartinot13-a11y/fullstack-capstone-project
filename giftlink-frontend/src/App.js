37e70a0aaa7a8df63209af81a2307eabee72efd0
Herve MARTINOT <h.martinot13@gmail.com>
2026-05-31 07:34:34 -0400

    module 3 front end step3

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -18,5 +18,29 @@ function App() {
         </>
   );
 }
+*/
+export default App;
 
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+const navigate = useNavigate();
+return (
+<>
+<Navbar/>
+<Routes>
+<Route path="/" element={<MainPage />} />
+<Route path="/app" element={<MainPage />} />
+<Route path="/app/login" element={<LoginPage/>} />
+<Route path="/app/register" element={<RegisterPage />} />
+</Routes>
+</>
+);
+}
 export default App;
